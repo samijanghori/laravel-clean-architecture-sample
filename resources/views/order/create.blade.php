41c3c4d5b2cc2fbd2a6654f1ff796d606b020f32
unknown <smimhd12345@gmail.com>
2026-01-21 14:56:25 +0430

    Initial Laravel setup with order form UI

---
+++ resources/views/order/create.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Create Order</title>
+</head>
+<body>
+    <h1>Create Order</h1>
+
+    <form method="POST" action="#">
+        <input type="number" placeholder="Amount">
+        <select>
+            <option>Online</option>
+            <option>Cash</option>
+        </select>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// routes/web.php
 Route::get('/', function () {
-    return view('welcome');
+    return view('order.create');
 });
